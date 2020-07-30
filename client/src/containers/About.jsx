7fc74ecc9dbb94db7fe9f49e24303ae81c213789
John Kang <pollyfactor@hotmail.com>
2020-07-30 00:28:10 -0400

    removed unused images and added spans to card

--- client/src/containers/About.jsx
+++ client/src/containers/About.jsx
@@ -6,14 +6,14 @@ const About = () => {
     <>
       <section className="hero is-primary is-fullheight-with-navbar has-text-light">
         <div className="hero-body">
-          <div className="container">
+          <div className="container is-mobile is-centered">
             <div className="row">
-              <div className="columns is-mobile is-centered">
+              <div className="columns">
                 <div className="column"></div>
                 <div className="column is-half">
                   <div className="card">
                     <div className="card-content">
-                      <p className="title has-text-dark has-text-right">
+                      <p className="title has-text-dark has-text-justified">
                         As a kid, I would make websites for fun. Considering how
                         much our lives relate to technology, I want to be part
                         of the future. So I can join the expanding network of
@@ -36,46 +36,58 @@ const About = () => {
                             <p className="subtitle is-5 has-text-dark">
                               Atlanta, Georgia
                             </p>
-                            <i
-                              class="fa fa-phone-square"
-                              aria-hidden="true"
-                            ></i>
-                            <span> 678-616-1566</span>
-                            <p>Email: pollyfactor@hotmail.com</p>
+
                             <p>
-                              Github:{" "}
-                              <a
-                                href="https://github.com/drivelikejehu"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                github.com/drivelikejehu
-                              </a>
+                              <i
+                                class="fa fa-phone-square"
+                                aria-hidden="true"
+                              ></i>
+                              <span> 678-616-1566</span>
                             </p>
                             <p>
-                              LinkedIn:{" "}
-                              <a
-                                href=" https://www.linkedin.com/in/drivelikejehu"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                linkedin.com/in/drivelikejehu/
-                              </a>
+                              <i class="fa fa-envelope" aria-hidden="true"></i>
+                              <span> pollyfactor@hotmail.com</span>
                             </p>
 
-                            {/* <span>
-                              <span className="tag is-light">Html</span>
-                              <span className="tag is-light">CSS</span>
-                              <span className="tag is-light">JavaScript</span>
-                              <span className="tag is-light">jQuery</span>
-                              <span className="tag is-light">Bootstrap</span>
-                              <span className="tag is-light">JavaScript</span>
-                            </span> */}
+                            <p>
+                              <i class="fab fa-github-square"></i>
+                              <span>
+                                <a
+                                  href="https://github.com/drivelikejehu"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  {" "}
+                                  github.com/drivelikejehu
+                                </a>
+                              </span>
+                            </p>
+
+                            <p>
+                              <i class="fab fa-linkedin"></i>
+                              <span>
+                                <a
+                                  href=" https://www.linkedin.com/in/drivelikejehu"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  {" "}
+                                  linkedin.com/in/drivelikejehu/
+                                </a>
+                              </span>
+                            </p>
                           </div>
+                        </div>
+                        <div className="card-footer-item">
+                          <span className="tag is-light">Html</span>
+                          <span className="tag is-light">CSS</span>
+                          <span className="tag is-light">JavaScript</span>
+                          <span className="tag is-light">jQuery</span>
+                          <span className="tag is-light">Bootstrap</span>
+                          <span className="tag is-light">JavaScript</span>
                         </div>
                       </div>
                     </div>
-                    {/* <div className="card-content"></div> */}
                   </div>
                 </div>
                 <div className="column"></div>
